ab9feb0108ea19dbe5732362cc6e644372b50cb1
Dominic981 <111631356+Dominic981@users.noreply.github.com>
2022-12-19 14:40:24 +0000

    Update i18n.js

--- modules/i18n.js
+++ modules/i18n.js
@@ -1,7 +1,7 @@
 // Perform substitutions for i18n in text and attributes
 export function apply_i18n(doc) {
     for(let node of doc.querySelectorAll('[data-i18n]')) {
-        let text = browser.i18n.getMessage(node.dataset.i18n) || node.dataset.i18n;
+        let text = chrome.i18n.getMessage(node.dataset.i18n) || node.dataset.i18n;
         if(node.dataset.i18nAllowMarkup !== undefined) {
             node.insertAdjacentHTML('beforeend', text);
         } else {
@@ -11,7 +11,7 @@ export function apply_i18n(doc) {
     for(let node of doc.querySelectorAll('[data-i18n-attrs]')) {
         for(let substitution of node.dataset.i18nAttrs.trim().split(/\s+/g)) {
             let [attr, text] = substitution.split(':');
-            text = browser.i18n.getMessage(text) || text;
+            text = chrome.i18n.getMessage(text) || text;
             node.setAttribute(attr, text);
         }
     }
